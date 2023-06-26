cb7072d6f6e196bc4a7c8cb774773d99e3bdc5da
Lipartiaana <124500573+Lipartiaana@users.noreply.github.com>
2023-06-26 16:58:15 +0400

    feature/mainSectionUpdate
    adding illustrationData.js for illustrations array,  adding modal closing functionality

---
+++ js/illustrationsData.js
@@ -0,0 +1,52 @@
+const illustationsArray = [
+  {
+    image: "./assets/illustrations/1.jpg",
+    title: "Illustration Title",
+    description:
+      "Short text short text short text short text short text short text short text short text",
+  },
+  {
+    image: "./assets/illustrations/2.jpg",
+    title: "Illustration Title",
+    description:
+      "Short text short text short text short text short text short text short text short text",
+  },
+  {
+    image: "./assets/illustrations/3.jpg",
+    title: "Illustration Title",
+    description:
+      "Short text short text short text short text short text short text short text short text",
+  },
+  {
+    image: "./assets/illustrations/4.jpg",
+    title: "Illustration Title",
+    description:
+      "Short text short text short text short text short text short text short text short text",
+  },
+  {
+    image: "./assets/illustrations/5.jpg",
+    title: "Illustration Title",
+    description:
+      "Short text short text short text short text short text short text short text short text",
+  },
+  {
+    image: "./assets/illustrations/6.jpg",
+    title: "Illustration Title",
+    description:
+      "Short text short text short text short text short text short text short text short text",
+  },
+  {
+    image: "./assets/illustrations/7.jpg",
+    title: "Illustration Title",
+    description:
+      "Short text short text short text short text short text short text short text short text",
+  },
+  {
+    image: "./assets/illustrations/8.jpg",
+    title: "Illustration Title",
+    description:
+      "Short text short text short text short text short text short text short text short text",
+  },
+];
+
+export default illustationsArray;
