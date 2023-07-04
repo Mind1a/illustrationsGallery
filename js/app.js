f87e747dced6467dcc9d832a3b2a187110161fdc
Lipartiaana <124500573+Lipartiaana@users.noreply.github.com>
2023-07-04 17:37:46 +0400

    feature/pagination
    adding pagination

--- js/app.js
+++ js/app.js
@@ -3,6 +3,8 @@ const toggleBtns = document.querySelector(".toggle-btns");
 const burgerMenuBtn = document.querySelector(".burger-menu-btn");
 const menu = document.querySelector(".menu");
 const closeMenuBtn = document.querySelector(".close");
+const galleryContainer = document.querySelector(".gallery-container");
+const paginationContainer = document.getElementById("pagination");
 
 toggleMenuBtn.addEventListener("click", () => {
   toggleBtns.classList.toggle("active");
--- js/main.js
+++ js/main.js
@@ -1,18 +1,15 @@
-import illustationsArray from "./illustrationsData.js";
+// Generates illustration card
 
-// Generates illustration cards
-
-illustationsArray.forEach((obj) => {
+export function createCard(obj) {
   let galleryCard = document.createElement("div");
   let galleryCardHTML = `
          <button popovertarget="gallery-popover">
           <img src="${obj.image}" alt="${obj.title}" />
         </button>; `;
 
-  const galleryContainer = document.querySelector(".gallery-container");
   galleryCard.innerHTML = galleryCardHTML;
   galleryCard.addEventListener("click", function () {
     openModal(obj);
   });
   galleryContainer.appendChild(galleryCard);
-});
+}
